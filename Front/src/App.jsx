09ff865bc145c4be6117f07d83a2e933ba09a929
douglas85rj <douglas85rj@gmail.com>
2023-05-21 01:59:18 -0300

    Sleep

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -16,35 +16,61 @@ import { useEffect, useState } from "react";
 import ModalComp from "./components/ModalComp";
 import axios from "axios";
 
-
-
 const App = () => {
-
   //get
   const getEmployees = async () => {
-    try{
-    const response = await axios.get("http://localhost:3333/employees");
-  return response.data;
+    try {
+      const response = await axios.get("http://localhost:3333/employees");
+      return response.data;
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   useEffect(() => {
     getEmployees().then((data) => setData(data));
   }, []);
 
   //update
-  const putEmployees = async (dataEdit) => {
-    try{
-    const response = await axios.put("http://localhost:3333/employees", setDataEdit);
-  return response.data;
+  const updateEmployees = async (dataEdit) => {
+    try {
+      const response = await axios.put(
+        "http://localhost:3333/employees",
+        setDataEdit
+      );
+      return response.data;
     } catch (error) {
       console.error(error);
     }
-  }
+  };
   useEffect(() => {
-    putEmployees().then((dataEdit) => setData(employees));
+    updateEmployees().then((dataEdit) => setData(employees));
   }, []);
+
+  //add
+  const addEmployees = async (data) => {
+    try {
+      const response = await axios.post( "http://localhost:3333/employees/add", setData);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    addEmployees().then((data) => setData(employees));
+  }, []);
+//delete
+const removeEmployees = async (data) => {
+    try {
+      const response = await axios.delete( "http://localhost:3333/employees", setData);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    removeEmployees(id).then((data) => setData(employees));
+  }, []);
+
 
 
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -70,22 +96,13 @@ const App = () => {
       justify="center"
       fontSize="20px"
       fontFamily="poppins"
-    > 
+    >
       <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
-    
         <Flex mb="8" justify="space-between" align="center">
-          <Button
-            as="a"
-            size="sm"
-            fontSize="sm"
-            colorScheme="blue"
-            leftIcon={<EditIcon />}
-            onClick={onOpen}
-          >
-            Novo Cadastro
-          </Button>
+        <Button colorScheme="blue" onClick={() => [setDataEdit({}), onOpen()]}>
+          NOVO CADASTRO
+        </Button>
           <ModalComp
-
             data={data}
             setData={setData}
             dataEdit={dataEdit}
@@ -93,53 +110,35 @@ const App = () => {
             onClose={onClose}
           />
         </Flex>
-        
+
         <Table mt="6">
           <Thead>
             <Tr>
-            <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  Nome
-                </Th>
               <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  E-Mail
-                </Th>
-              {/* <Th>Telefone</Th>
-              <Th>CPF</Th>
-              <Th>RG</Th>
-              <Th>Endereço</Th>
-              <Th>CEP</Th>
-              <Th>UF</Th>
-              <Th>Cidade</Th>
-              <Th>Editar</Th>
-              <Th>Excluir</Th> */}
-               <Th p={0}></Th>
-                <Th p={0}></Th>
+                Nome
+              </Th>
+              <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                E-Mail
+              </Th>
+              <Th p={0}></Th>
+              <Th p={0}></Th>
             </Tr>
           </Thead>
           <Tbody>
             {data?.map((employees, index) => (
-                <Tr key={index} cursor="pointer " _hover={{ bg: "gray.100" }}>
+              <Tr key={index} cursor="pointer " _hover={{ bg: "gray.100" }}>
                 <Td maxW={isMobile ? 5 : 100}>{employees.name}</Td>
                 <Td maxW={isMobile ? 5 : 100}>{employees.email}</Td>
+                <Td p={0}></Td>
+
                 <Td p={0}>
-                {/* <Td>{employees.phone}</Td>
-                <Td>{employees.cpf}</Td>
-                <Td>{employees.rg}</Td>
-                <Td>{employees.address}</Td>
-                <Td>{employees.cep}</Td>
-                <Td>{employees.uf}</Td>
-                <Td>{employees.city}</Td> */}
-                </Td>
-
-
-               <Td p={0}>
-               <EditIcon
-                      fontSize={20}
-                      onClick={() => [
-                        setDataEdit({ ...employees, index }),
-                        onOpen(),
-                      ]}
-                    />
+                  <EditIcon
+                    fontSize={20}
+                    onClick={() => [
+                      setDataEdit({ ...employees, index }),
+                      onOpen(),
+                    ]}
+                  />
                 </Td>
                 <Td>
                   <Button
@@ -173,7 +172,3 @@ const App = () => {
 };
 
 export default App;
-
-
-
-
